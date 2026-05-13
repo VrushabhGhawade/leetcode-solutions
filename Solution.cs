67821c4358ed70e447a9611c57760233bfaaf531
VrushabhGhawade <110442370+VrushabhGhawade@users.noreply.github.com>
2026-05-13 13:03:35 +0530

    Clean up Solution.cs by removing comments
    Removed comments and formatting from Solution.cs.

--- Solution.cs
+++ Solution.cs
@@ -1,18 +1,3 @@
-# Intuition
-<!-- Describe your first thoughts on how to solve this problem. -->
-
-# Approach
-<!-- Describe your approach to solving the problem. -->
-
-# Complexity
-- Time complexity:
-<!-- Add your time complexity here, e.g. $$O(n)$$ -->
-
-- Space complexity:
-<!-- Add your space complexity here, e.g. $$O(n)$$ -->
-
-# Code
-```csharp []
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         Dictionary<int, int> map = new Dictionary<int, int>();
@@ -30,4 +15,3 @@ public class Solution {
         return new int[] {};
     }
 }
-```
